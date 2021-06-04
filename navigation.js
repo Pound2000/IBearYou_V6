d9f386db0dd6a15bbbaedabfc52d9efc58cdde98
Fang <aratitraya.atty@mail.kmutt.ac.th>
2021-06-05 00:57:00 +0700

    edit navigation

--- navigation.js
+++ navigation.js
@@ -1,13 +1,12 @@
-import React, {Component} from 'react';
+import * as React from 'react';
 import { Text, View, SafeAreaView,Image, TouchableOpacity, ScrollView,ActivityIndicator } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
 
-
 import login from './login';
-
+ 
 import checkup1 from './checkup1';
 import checkup2 from './checkup2';
 import checkup3 from './checkup3';
@@ -16,28 +15,253 @@ import checkup5 from './checkup5';
 import checkup6 from './checkup6';
 import checkup7 from './checkup7';
 import checkup8 from './checkup8';
-
+ 
 import resultScreen from './resultScreen';
 
-const StackApp = createStackNavigator();
 
-export default function navigation() {
-  
+function CustomHeader({title}) {
   return (
-    <NavigationContainer>
-     <Stack.Navigator initialRouteName="checkup1">
-      <StackApp.Screen name="checkup1" components={checkup1} />
-      <StackApp.Screen name="checkup2" components={checkup2} />
-      <StackApp.Screen name="checkup3" components={checkup3} />
-      <StackApp.Screen name="checkup4" components={checkup4} />
-      <StackApp.Screen name="checkup5" components={checkup5} />
-      <StackApp.Screen name="checkup6" components={checkup6} />
-      <StackApp.Screen name="checkup7" components={checkup7} />
-      <StackApp.Screen name="checkup8" components={checkup8} />
+    <View style={{flexDirection: 'row',height: 50, borderWidth: 1, borderColor: 'pink'}}>
 
-      <StackApp.Screen name="resultScreen" components={resultScreen} />
-     </Stack.Navigator>
-    </NavigationContainer>
+  <View style={{flex: 1.5, justifyContent: 'center', borderWidth: 1, borderColor: 'pink'}}>
+    <Text style={{textAlign: 'center'}}>I Bear You</Text>
+    </View>
+     </View>
   );
 }
 
+
+function HomeScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <CustomHeader title='Home'/>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home!</Text>
+          <TouchableOpacity
+        style={{marginTop: 20}}
+        >
+        <Text> Go to graph </Text>
+      </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+function Graph() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <CustomHeader title='Graph'/>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home!</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+function CalendarScreen() {
+  return (
+     <SafeAreaView style={{ flex: 1 }}>
+     <CustomHeader title='Calendar' />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Calendar!</Text>
+        <TouchableOpacity
+        style={{marginTop: 20}}
+        >
+        <Text> Let's Get Started </Text>
+      </TouchableOpacity>
+    </View>
+     </SafeAreaView>
+  );
+}
+
+function MoodScreen() {
+  return (
+     <SafeAreaView style={{ flex: 1 }}>
+     <CustomHeader title='Mood' />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>How do you feel?</Text>
+        <TouchableOpacity
+        style={{marginTop: 20}}
+        >
+        <Text> Let's Get Started </Text>
+      </TouchableOpacity>
+    </View>
+     </SafeAreaView>
+  );
+}
+
+function DairyScreen() {
+  return (
+     <SafeAreaView style={{ flex: 1 }}>
+     <CustomHeader title='Dairy' />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>What happened today?!</Text>
+        <TouchableOpacity
+        style={{marginTop: 20}}
+        >
+        <Text> Let's Get Started </Text>
+      </TouchableOpacity>
+    </View>
+     </SafeAreaView>
+  );
+}
+
+function CheckupScreen() {
+  return (
+     <SafeAreaView style={{ flex: 1 }}>
+         <CustomHeader title='Checkup'/>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Checkup!</Text>
+          <TouchableOpacity
+        style={{marginTop: 20}}
+        >
+        <Text> Let's Get Started </Text>
+      </TouchableOpacity>
+    </View>
+     </SafeAreaView>
+  );
+}
+
+function StartCheckupScreen() {
+  return (
+     <SafeAreaView style={{ flex: 1 }}>
+         <CustomHeader title='StartCheckup'/>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Checkup!</Text>
+          <TouchableOpacity
+        style={{marginTop: 20}}
+        >
+        <Text> Next </Text>
+      </TouchableOpacity>
+    </View>
+     </SafeAreaView>
+  );
+}
+
+function VoiceScreen() {
+  return (
+     <SafeAreaView style={{ flex: 1 }}>
+      <CustomHeader title='Voice' />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Voice!</Text>
+            <TouchableOpacity
+        style={{marginTop: 20}}
+        >
+        <Text> Start Record  </Text>
+      </TouchableOpacity>
+    </View>
+     </SafeAreaView>
+  );
+}
+
+function AlarmScreen() {
+  return (
+     <SafeAreaView style={{ flex: 1 }}>
+      <CustomHeader title='Alarm' />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Alarm Setting!</Text>
+            <TouchableOpacity
+        style={{marginTop: 20}}
+        >
+        <Text> Start Alarm setting  </Text>
+      </TouchableOpacity>
+    </View>
+     </SafeAreaView>
+  );
+}
+
+function ProfileScreen() {
+  return (
+     <SafeAreaView style={{ flex: 1 }}>
+      <CustomHeader title='Profile' />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile!</Text>
+           <TouchableOpacity
+        style={{marginTop: 20}}
+        >
+        <Text> Setting  </Text>
+      </TouchableOpacity>
+    </View>
+     </SafeAreaView>
+  );
+}
+
+
+function SettingScreen() {
+  return (
+     <SafeAreaView style={{ flex: 1 }}>
+      <CustomHeader title='Profile' />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Setting!</Text>
+    </View>
+     </SafeAreaView>
+  );
+}
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator initialRouteName='Home'>
+      <Stack.Screen name='Home' component={HomeScreen}/>
+      <Stack.Screen name='Graph' component={Graph}/>
+     </Stack.Navigator>
+
+  )
+}
+
+function DairyStack() {
+  return (
+    <Stack.Navigator initialRouteName='Calendar'>
+      <Stack.Screen name='Calendar' component={CalendarScreen}/>
+      <Stack.Screen name='Dairy' component={DairyScreen}/>
+      <Stack.Screen name='Mood' component={MoodScreen}/>
+     </Stack.Navigator>
+
+  )
+}
+
+function CheckupStack() {
+  return (
+    <Stack.Navigator initialRouteName='Checkup'>
+      <Stack.Screen name='Checkup' component={CheckupScreen}/>
+      <Stack.Screen name='StartCheckup' component={StartCheckupScreen}/>
+     </Stack.Navigator>
+
+  )
+}
+
+function BearStack() {
+  return (
+    <Stack.Navigator initialRouteName='Voice'>
+      <Stack.Screen name='Voice' component={VoiceScreen}/>
+      <Stack.Screen name='Alarm' component={AlarmScreen}/>
+     </Stack.Navigator>
+
+  )
+}
+
+function Profiletack() {
+  return (
+    <Stack.Navigator initialRouteName='Profile'>
+      <Stack.Screen name='Profile' component={ProfileScreen}/>
+      <Stack.Screen name='Setting' component={Graph}/>
+     </Stack.Navigator>
+
+  )
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Dairy" component={CalendarScreen} />
+        <Tab.Screen name="Checkup" component={CheckupScreen} />
+        <Tab.Screen name="Voice" component={VoiceScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
