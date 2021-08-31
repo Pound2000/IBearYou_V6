4472ae919a827542b40492e1fb022312a1a6a499
Pound2000 <pound799@gmail.com>
2021-08-31 21:47:33 +0700

    [Create] TestFetch

---
+++ TestFetch.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getUser = async () => {
+     try {
+      const response = await fetch('http://localhost:3000/User');
+      const json = await response.json();
+      setData(json.user);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  return (
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ userId }, index) => userId}
+          renderItem={({ item }) => (
+            <Text>{item.firstName}   {item.lastName} <br></br>
+            {item.email}</Text>
+          )}
+        />
+      )}
+    </View>
+  );
+};
