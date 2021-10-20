6ec939345a1178799bdf5b2730ad4ac0827dce6d
atty <Aratitraya.atty@mail.kmutt.ac.th>
2021-10-21 00:32:18 +0700

    [Edit] calendar_MarkedDate

--- CalendarScreen.js
+++ CalendarScreen.js
@@ -7,13 +7,13 @@ import Bg2 from './components/Bg2';
 
 import CustomHeader from './CustomHeader';
 import {Calendar, LocaleConfig} from 'react-native-calendars';
-  /*LocaleConfig.locales['ts'] = {
+  /*LocaleConfig.locales['th'] = {
     monthNames: ['มกราคม','กุมภาพันธ์','มีนาคม','เมษายน','พฤษภาคม','มิถุนายน','กรกฎาคม','สิงหาคม','กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม'],
     monthNamesShort: ['ม.ค.','ก.พ.','มี.ค.','เม.ย.','พ.ค.','มิ.ย.','ก.ค.','ส.ค.','ก.ย.','ต.ค.','พ.ย.','ธ.ค.'],
-    dayNames: ['จันทร์','อังคาร','พุธ','พฤหัสบดี','ศุกร์','เสาร์','อาทิตย์'],
-    dayNamesShort: ['จ.','อ.','พ.','พฤ.','ศ.','ส.','อา.']
+    dayNames: ['อาทิตย์','จันทร์','อังคาร','พุธ','พฤหัสบดี','ศุกร์','เสาร์'],
+    dayNamesShort: ['อา.','จ.','อ.','พ.','พฤ.','ศ.','ส.']
   }
-  LocaleConfig.defaultLocale = 'tr';*/
+  LocaleConfig.defaultLocale = 'th';*/
 
 class CalendarScreen extends Component {
   constructor(props) {
@@ -31,17 +31,73 @@ class CalendarScreen extends Component {
 
 <View>
   <Calendar
+  
     theme={{calendarBackground: '#E79995',
-            dayTextColor: '#000000',
+            dayTextColor: '#000000', textDayFontFamily: 'Quark', textDayFontSize: 16,
             todayTextColor: '#FFFFFF',
-            monthTextColor: '#000000',
-            selectedDayBackgroudColor: '#CB6863',
+            monthTextColor: '#000000', textMonthFontSize: 18,textMonthFontFamily: 'Quark',
+            selectedDayBackgroudColor: '#CB6863', arrowColor: '#000000',
             selectedDayTextColor: '#FFFFFF',
-            textSectionTitleColor: '#000000',
+            textSectionTitleColor: '#000000',  textDayHeaderFontWeight: 'bold', textDayHeaderFontFamily: 'Quark', textDayHeaderFontSize: 16,
             textDisabledColor: '#FFFFFF',
-            arrowColor: '#000000',
+            'stylesheet.calendar.header': {
+    week: {
+      marginTop: 5,
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+    }
+  }
     }}
-  style={styles.calendar}/>
+
+/*dayComponent={({date, state}) => {
+    return (
+      <View>
+        <Text style={{textAlign: 'center', color: state === 'disabled' ? '#FFFFFF' : '#000000'}}>
+          {date.day}
+        </Text>
+      </View>
+    );
+  }}*/
+
+  style={styles.calendar}
+  
+  markingType={'custom'}
+    markedDates={{
+    '2021-10-20': {selected: true, marked: true, selectedColor: '#CB6863'},
+    '2021-10-17': {selected: true,marked: true,},
+    '2021-10-18': {marked: true, dotColor: 'red', activeOpacity: 0},
+    '2021-10-19': {disabled: true, disableTouchEvent: true}
+
+  }}
+
+   /*markedDates={{
+    '2021-10-20': {
+      customStyles: {
+        container: {
+          backgroundColor: 'green'
+        },
+        text: {
+          color: 'black',
+          fontWeight: 'bold'
+        }
+      }
+    },
+    '2021-10-18': {
+      customStyles: {
+        container: {
+          backgroundColor: 'white',
+          elevation: 2
+        },
+        text: {
+          color: 'blue'
+        }
+      }
+    }
+  }}*/
+
+  
+  />
+
 
 
 
@@ -49,12 +105,31 @@ class CalendarScreen extends Component {
 
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
 
-    <View style={{marginTop:20}}>
+    <View style={{marginTop:95}}>
       <Text style={styles.textName}>Hi Riboots !</Text>
       <View style={styles.boxContent}>
       <Text style={styles.content}>ในหนึ่งวันต้องพบเจอเรื่องราวมากมาย หลากหลายความรู้สึก สมุดเล่มนี้จะเก็บบันทึกเรื่องราวต่าง ๆ 
       ความรู้สึกที่อยากบันทึกไว้ หรือจะเขียนตั้งเป้าหมายก้ได้ ถ้าพร้อมแล้วไปเขียนบันทึกในแบบฉบับของตัวเองกันเลย</Text>
       </View>
+
+
+
+      <View style={{flex: 1, alignItems: 'center',}}>  
+       <Image source={require('./assets/images/boots.png')}
+    style={{width:50.91,height:41.96,marginTop:-20,marginRight:-320}} />     
+ </View>
+
+   <View style={{flex: 1, alignItems: 'center',}}>  
+       <Image source={require('./assets/images/umbrella-blue.png')}
+    style={{width:135.42 ,height:111,marginTop: -235,marginRight: -180}} />     
+ </View>
+
+ <View style={{flex: 1, alignItems: 'center',}}>  
+       <Image source={require('./assets/images/raincoat.png')}
+    style={{width:98.08 ,height:107.17,marginTop: -75,marginLeft: -280}} />     
+ </View>
+     </View>
+    </View>
 
  <View style = {styles.button}>
           <TouchableOpacity
@@ -67,22 +142,6 @@ class CalendarScreen extends Component {
       </TouchableOpacity>
     </View>
 
-      <View style={{flex: 1, alignItems: 'center',}}>  
-       <Image source={require('./assets/images/boots.png')}
-    style={{width:50.91,height:41.96,marginTop:-45,marginRight:-320}} />     
- </View>
-
-   <View style={{flex: 1, alignItems: 'center',}}>  
-       <Image source={require('./assets/images/umbrella-blue.png')}
-    style={{width:135.42 ,height:111,marginTop: -290,marginRight: -180}} />     
- </View>
-
- <View style={{flex: 1, alignItems: 'center',}}>  
-       <Image source={require('./assets/images/raincoat.png')}
-    style={{width:98.08 ,height:107.17,marginTop: -140,marginLeft: -280}} />     
- </View>
-     </View>
-    </View>
      </SafeAreaView>
     );
   }
@@ -93,14 +152,14 @@ const styles = StyleSheet.create({
       fontSize: 18,
       fontFamily: 'Quark',
       textAlign: 'center',
-      marginTop: 30,
+      marginTop: 20,
       paddingLeft: 30,
       paddingRight: 30,
     },
 
     boxContent:{
       width: 318,
-      height: 170,
+      height: 160,
       backgroundColor: '#FFFFFF',
       borderRadius: 10,
       shadowColor: '#E79995',
@@ -131,7 +190,7 @@ const styles = StyleSheet.create({
        backgroundColor: '#E79995',
        height: 41,
        width: 278,
-       marginTop: -30,
+       marginTop: -45,
        overflow: "hidden",
       
       
@@ -154,16 +213,15 @@ const styles = StyleSheet.create({
    },
 
     calendar: {
-      //borderBottomRightRadius: 25,
-      //borderBottomLeftRadius: 25,
+      borderBottomRightRadius: 20,
+      borderBottomLeftRadius: 20,
       shadowColor: '#000000',
       shadowOffset: { width: 0, height: 4 },
       shadowRadius: 3,
       shadowOpacity: 0.4,
       elevation: 3,
-
-   
-      
+      paddingLeft: 25,
+      paddingRight: 25,
     }
 
   });
